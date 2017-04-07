fe5d9bfa4d50a3cdd8bc2c8581bc27a2c71a7777
Katie Reed <katharynreedpierce@gmail.com>
2017-04-07 09:15:27 -0700

    create about component and route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomepageComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
