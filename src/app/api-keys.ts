583fd32ad136ca517ce2cb89ef160c44601fba4c
Katie Reed <katharynreedpierce@gmail.com>
2017-04-07 10:12:26 -0700

    add firebase integration to app

---
+++ src/app/api-keys.ts
@@ -0,0 +1,6 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyBWghuRv0rdB2p5e4JO9je1NLfo1z5Vr24",
+  authDomain: "epicurldus.firebaseapp.com",
+  databaseURL: "https://epicurldus.firebaseio.com",
+  messagingSenderId: "39631042645"
+  };
